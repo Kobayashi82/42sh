afba72604b9ae59c384bf78ec03cf5e1c5a9ab07
Kobayashi82 <kobayashi82@outlook.com>
2025-11-22 20:25:41 +0100

    Arreglados algunos segfaults... pero aún queda

--- inc/parser/lexer.h
+++ inc/parser/lexer.h
@@ -6,14 +6,14 @@
 /*   By: vzurera- <vzurera-@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/22 12:14:29 by vzurera-          #+#    #+#             */
-/*   Updated: 2025/11/22 19:58:46 by vzurera-         ###   ########.fr       */
+/*   Updated: 2025/11/22 20:24:32 by vzurera-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #pragma once
 
 typedef enum e_lx_status { LX_COMPLETE, LX_INCOMPLETE, LX_FAILED } t_lx_status;
-typedef enum e_type { LX_WORD, LX_PIPE, LX_AND, LX_OR, LX_BRACE, LX_BRACE_GROUP, LX_VAR, LX_CMD_SHELL, LX_SHELL, LX_CMD_ARIT, LX_ARIT, LX_SEMICOLON, LX_REDIR_HEREDOC, LX_REDIR_IN, LX_REDIR_OUT, LX_REDIR_APPEND, LX_REDIR_HERESTRING } t_lx_type;
+typedef enum e_type { LX_WORD, LX_PIPE, LX_BACKGROND, LX_AND, LX_OR, LX_BRACE, LX_BRACE_GROUP, LX_VAR, LX_CMD_SHELL, LX_SHELL, LX_CMD_ARIT, LX_ARIT, LX_SEMICOLON, LX_REDIR_HEREDOC, LX_REDIR_IN, LX_REDIR_OUT, LX_REDIR_APPEND, LX_REDIR_HERESTRING } t_lx_type;
 
 typedef struct s_lexer {
 	t_lx_type		type;
