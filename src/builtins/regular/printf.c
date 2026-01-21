5f0735d182553af2b4a39078192512b99dc0c6d9
Kobayashi82 <kobayashi82@outlook.com>
2026-01-21 19:57:05 +0100

    Version 2 de printf

--- src/builtins/regular/printf.c
+++ src/builtins/regular/printf.c
@@ -6,7 +6,7 @@
 /*   By: vzurera- <vzurera-@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/16 12:08:17 by vzurera-          #+#    #+#             */
-/*   Updated: 2026/01/21 19:23:47 by vzurera-         ###   ########.fr       */
+/*   Updated: 2026/01/21 19:56:30 by vzurera-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,6 +24,17 @@
 
 	static char		g_output_buffer[BUFFER_SIZE];
 	static size_t	g_buffer_pos = 0;
+
+	typedef struct s_fmt_token {
+		int			is_spec;
+		const char	*lit;
+		size_t		lit_len;
+		int			flags;
+		int			width;
+		int			precision;
+		int			length;
+		char		spec;
+	} t_fmt_token;
 
 #pragma endregion
 
@@ -129,8 +140,8 @@
 
 		#pragma region "Char"
 
-			static void	buffer_char(char c) {
-				if (g_buffer_pos >= BUFFER_SIZE - 1) {
+			static void buffer_char(char c) {
+				if (g_buffer_pos >= BUFFER_SIZE) {
 					write(STDOUT_FILENO, g_output_buffer, g_buffer_pos);
 					g_buffer_pos = 0;
 				}
@@ -141,16 +152,26 @@
 
 		#pragma region "String"
 
-			static void	buffer_string(const char *str, size_t len) {
-				size_t	i;
+			static void buffer_string(const char *str, size_t len) {
+				size_t	space;
+				size_t	chunk;
 
-				i = 0;
-				while (i < len) {
-					if (g_buffer_pos >= BUFFER_SIZE - 1) {
+				if (!str || len == 0) return;
+				while (len > 0) {
+					if (g_buffer_pos >= BUFFER_SIZE) {
 						write(STDOUT_FILENO, g_output_buffer, g_buffer_pos);
 						g_buffer_pos = 0;
 					}
-					g_output_buffer[g_buffer_pos++] = str[i++];
+					space = BUFFER_SIZE - g_buffer_pos;
+					chunk = (len < space) ? len : space;
+					memcpy(g_output_buffer + g_buffer_pos, str, chunk);
+					g_buffer_pos += chunk;
+					str += chunk;
+					len -= chunk;
+					if (g_buffer_pos == BUFFER_SIZE) {
+						write(STDOUT_FILENO, g_output_buffer, g_buffer_pos);
+						g_buffer_pos = 0;
+					}
 				}
 			}
 
@@ -158,7 +179,7 @@
 
 		#pragma region "Flash"
 
-			static void	flush_buffer() {
+			static void flush_buffer() {
 				if (g_buffer_pos > 0) {
 					write(STDOUT_FILENO, g_output_buffer, g_buffer_pos);
 					g_buffer_pos = 0;
@@ -173,7 +194,7 @@
 
 		#pragma region "Flags"
 
-			static void	parse_flags(const char **fmt, int *flags) {
+			static void parse_flags(const char **fmt, int *flags) {
 				*flags = 0;
 				while (**fmt) {
 					if		(**fmt == '-')		*flags |= 1;
@@ -190,7 +211,7 @@
 
 		#pragma region "Width"
 
-			static int	parse_width(const char **fmt) {
+			static int parse_width(const char **fmt) {
 				int	width = 0;
 
 				while (isdigit(**fmt)) {
@@ -205,7 +226,7 @@
 
 		#pragma region "Precision"
 
-			static int	parse_precision(const char **fmt) {
+			static int parse_precision(const char **fmt) {
 				int precision;
 
 				if (**fmt != '.') return (-1);
@@ -221,13 +242,136 @@
 
 		#pragma endregion
 
+		#pragma region "Length"
+
+			static int parse_length(const char **fmt) {
+				if (**fmt == 'l' && *(*fmt + 1) == 'l') {
+					(*fmt) += 2;
+					return (2);
+				}
+				if (**fmt == 'l') {
+					(*fmt)++;
+					return (1);
+				}
+
+				return (0);
+			}
+
+		#pragma endregion
+
+		#pragma region "Signed Len"
+
+			static long long parse_signed_len(const char *arg, int length, int *ok) {
+				char		*endptr;
+				long long	val;
+
+				errno = 0;
+				if (length == 2)	val = strtoll(arg, &endptr, 10);
+				else				val = (long long)strtol(arg, &endptr, 10);
+				if (*arg && (*endptr != '\0' || errno == ERANGE))	*ok = 0;
+				else												*ok = 1;
+
+				return (val);
+			}
+
+		#pragma endregion
+
+		#pragma region "Unsigned Len"
+
+			static unsigned long long parse_unsigned_len(const char *arg, int length) {
+				if (length == 2) return (strtoull(arg, NULL, 10));
+
+				return ((unsigned long long)strtoul(arg, NULL, 10));
+			}
+
+		#pragma endregion
+
+		#pragma region "Format"
+
+			static int append_token(t_fmt_token **tokens, size_t *count, size_t *cap, t_fmt_token *tok) {
+				t_fmt_token	*new_tokens;
+				size_t		new_cap;
+
+				if (*count >= *cap) {
+					new_cap = (*cap == 0) ? 16 : (*cap * 2);
+					new_tokens = realloc(*tokens, new_cap * sizeof(*new_tokens));
+					if (!new_tokens) return (1);
+					*tokens = new_tokens;
+					*cap = new_cap;
+				}
+				(*tokens)[(*count)++] = *tok;
+
+				return (0);
+			}
+
+			static int parse_format(const char *fmt, t_fmt_token **tokens, size_t *count, int *has_specs) {
+				size_t		cap;
+				const char	*p;
+				const char	*lit_start;
+				t_fmt_token	tok;
+				const char	*spec_ptr;
+
+				*count = 0;
+				*has_specs = 0;
+				cap = 0;
+				*tokens = NULL;
+				p = fmt;
+				lit_start = p;
+				while (*p) {
+					if (*p == '%') {
+						if (p > lit_start) {
+							tok.is_spec = 0;
+							tok.lit = lit_start;
+							tok.lit_len = (size_t)(p - lit_start);
+							if (append_token(tokens, count, &cap, &tok)) return (1);
+						}
+						if (*(p + 1) == '%') {
+							tok.is_spec = 1;
+							tok.flags = 0;
+							tok.width = 0;
+							tok.precision = -1;
+							tok.length = 0;
+							tok.spec = '%';
+							if (append_token(tokens, count, &cap, &tok)) return (1);
+							p += 2;
+							lit_start = p;
+							continue;
+						}
+						spec_ptr = p + 1;
+						parse_flags(&spec_ptr, &tok.flags);
+						tok.width = parse_width(&spec_ptr);
+						tok.precision = parse_precision(&spec_ptr);
+						tok.length = parse_length(&spec_ptr);
+						tok.spec = *spec_ptr;
+						tok.is_spec = 1;
+						if (append_token(tokens, count, &cap, &tok)) return (1);
+						if (tok.spec && tok.spec != '%') *has_specs = 1;
+						if (*spec_ptr) spec_ptr++;
+						p = spec_ptr;
+						lit_start = p;
+						continue;
+					}
+					p++;
+				}
+				if (p > lit_start) {
+					tok.is_spec = 0;
+					tok.lit = lit_start;
+					tok.lit_len = (size_t)(p - lit_start);
+					if (append_token(tokens, count, &cap, &tok)) return (1);
+				}
+
+				return (0);
+			}
+
+		#pragma endregion
+
 	#pragma endregion
 
 	#pragma region "Print"
 
 		#pragma region "Num to Str"
 
-			static int	num_to_str(unsigned long num, char *buffer, int base, int uppercase) {
+			static int num_to_str(unsigned long long num, char *buffer, int base, int uppercase) {
 				const char	*digits;
 				char		*ptr;
 				int			len;
@@ -254,12 +398,16 @@
 
 		#pragma region "Padding"
 
-			static void	print_padding(int count, char pad_char) {
-				int i = 0;
+			static void print_padding(int count, char pad_char) {
+				char	pads[64];
+				int		chunk;
 
-				while (i < count) {
-					buffer_char(pad_char);
-					i++;
+				if (count <= 0) return;
+				memset(pads, pad_char, sizeof(pads));
+				while (count > 0) {
+					chunk = (count > (int)sizeof(pads)) ? (int)sizeof(pads) : count;
+					buffer_string(pads, (size_t)chunk);
+					count -= chunk;
 				}
 			}
 
@@ -267,18 +415,20 @@
 
 		#pragma region "Int"
 
-			static void	print_int(long num, int width, int precision, int flags) {
+			static void print_int(long long num, int width, int precision, int flags) {
 				char	buffer[64];
 				int		len;
 				char	sign;
 				int		pad_char;
 				int		is_negative;
 				int		total_len;
+				unsigned long long	abs_num;
 
 				is_negative = (num < 0);
-				if (is_negative)					num = -num;
-				if (num == 0 && precision == 0)		len = 0;
-				else								len = num_to_str((unsigned long)num, buffer, 10, 0);
+				if (is_negative)						abs_num = (unsigned long long)(-(num + 1)) + 1;
+				else									abs_num = (unsigned long long)num;
+				if (abs_num == 0 && precision == 0)		len = 0;
+				else									len = num_to_str(abs_num, buffer, 10, 0);
 
 				while (len < precision) {
 					memmove(buffer + 1, buffer, len + 1);
@@ -287,9 +437,9 @@
 				}
 
 				sign = 0;
-				if		(is_negative)				sign = '-';
-				else if (flags & 2)					sign = '+';
-				else if (flags & 4)					sign = ' ';
+				if		(is_negative)	sign = '-';
+				else if (flags & 2)		sign = '+';
+				else if (flags & 4)		sign = ' ';
 
 				total_len = len + (sign ? 1 : 0);
 				pad_char = ((flags & 16) && precision < 0 && !(flags & 1)) ? '0' : ' ';
@@ -305,7 +455,7 @@
 
 		#pragma region "Unsigned"
 
-			static void	print_unsigned(unsigned long num, int base, int width, int precision, int flags, int uppercase) {
+			static void print_unsigned(unsigned long long num, int base, int width, int precision, int flags, int uppercase) {
 				char	buffer[64];
 				int		len;
 				int		pad_char;
@@ -345,7 +495,7 @@
 
 		#pragma region "Float"
 
-			static void	print_float(double num, int width, int precision, int flags) {
+			static void print_float(double num, int width, int precision, int flags) {
 				char	buffer[128];
 				int		len;
 				char	sign;
@@ -362,18 +512,17 @@
 				is_negative = (num < 0);
 				if (is_negative) num = -num;
 
-				int_part = (long)num;
+				int_part = (long long)num;
 				frac_part = num - int_part;
 				mult = 1;
-				for (i = 0; i < precision; i++)
-					mult *= 10;
+				for (i = 0; i < precision; i++) mult *= 10;
 				frac = (unsigned long long)(frac_part * (double)mult + 0.5);
 				if (frac >= mult) {
 					int_part++;
 					frac = 0;
 				}
 
-				len = num_to_str((unsigned long)int_part, buffer, 10, 0);
+				len = num_to_str((unsigned long long)int_part, buffer, 10, 0);
 				if (precision > 0) {
 					buffer[len++] = '.';
 					for (i = precision - 1; i >= 0; i--) {
@@ -385,9 +534,9 @@
 				buffer[len] = '\0';
 
 				sign = 0;
-				if		(is_negative)				sign = '-';
-				else if (flags & 2)					sign = '+';
-				else if (flags & 4)					sign = ' ';
+				if		(is_negative)	sign = '-';
+				else if (flags & 2)		sign = '+';
+				else if (flags & 4)		sign = ' ';
 
 				total_len = len + (sign ? 1 : 0);
 				pad_char = ((flags & 16) && !(flags & 1)) ? '0' : ' ';
@@ -401,9 +550,45 @@
 
 		#pragma endregion
 
+		#pragma region "Float Scientific"
+
+			static void print_float_scientific(double num, int width, int precision, int flags, char spec) {
+				char	fmt[32];
+				char	out[256];
+				int		pos;
+				int		n;
+				char	*dyn;
+
+				pos = 0;
+				fmt[pos++] = '%';
+				if (flags & 1)		fmt[pos++] = '-';
+				if (flags & 2)		fmt[pos++] = '+';
+				if (flags & 4)		fmt[pos++] = ' ';
+				if (flags & 8)		fmt[pos++] = '#';
+				if (flags & 16)		fmt[pos++] = '0';
+				if (width > 0)		pos += snprintf(fmt + pos, sizeof(fmt) - pos, "%d", width);
+				if (precision >= 0)	pos += snprintf(fmt + pos, sizeof(fmt) - pos, ".%d", precision);
+				fmt[pos++] = spec;
+				fmt[pos] = '\0';
+
+				n = snprintf(out, sizeof(out), fmt, num);
+				if (n < 0) return;
+				if (n < (int)sizeof(out)) {
+					buffer_string(out, (size_t)n);
+					return;
+				}
+				dyn = malloc((size_t)n + 1);
+				if (!dyn) return;
+				snprintf(dyn, (size_t)n + 1, fmt, num);
+				buffer_string(dyn, (size_t)n);
+				free(dyn);
+			}
+
+		#pragma endregion
+
 		#pragma region "String"
 
-			static void	print_string(const char *str, int width, int precision, int flags) {
+			static void print_string(const char *str, int width, int precision, int flags) {
 				int len;
 				int print_len;
 
@@ -424,7 +609,17 @@
 
 		#pragma region "Escapes"
 
-			static char	*process_escapes(const char *str) {
+			static int has_escape(const char *str) {
+				if (!str) return (0);
+				while (*str) {
+					if (*str == '\\') return (1);
+					str++;
+				}
+
+				return (0);
+			}
+
+			static char *process_escapes(const char *str) {
 				char		*result;
 				char		*dst;
 				const char	*src;
@@ -461,12 +656,9 @@
 							val = count = 0;
 							src++;
 							while (isxdigit(*src) && count < 2) {
-								if (*src >= '0' && *src <= '9')
-									val = val * 16 + (*src - '0');
-								else if (*src >= 'a' && *src <= 'f')
-									val = val * 16 + (*src - 'a' + 10);
-								else if (*src >= 'A' && *src <= 'F')
-									val = val * 16 + (*src - 'A' + 10);
+								if		(*src >= '0' && *src <= '9')	val = val * 16 + (*src - '0');
+								else if (*src >= 'a' && *src <= 'f')	val = val * 16 + (*src - 'a' + 10);
+								else if (*src >= 'A' && *src <= 'F')	val = val * 16 + (*src - 'A' + 10);
 								src++;
 								count++;
 							}
@@ -489,33 +681,31 @@
 
 		#pragma region "Specifier"
 
-			static int	process_specifier(const char **fmt, char **args, int *arg_idx, int arg_count) {
+			static int process_specifier_parsed(const t_fmt_token *tok, char **args, int *arg_idx, int arg_count) {
 				int			flags;
 				int			width;
 				int			precision;
+				int			length;
 				char		spec;
 				const char	*arg;
-				char		*endptr;
-				long		num;
+				long long	num;
+				int			ok;
 
-				(*fmt)++;
-				if (**fmt == '%') {
+				flags = tok->flags;
+				width = tok->width;
+				precision = tok->precision;
+				length = tok->length;
+				spec = tok->spec;
+				if (!spec) return (1);
+				arg = (*arg_idx < arg_count) ? args[*arg_idx] : "";
+				if (spec == '%') {
 					buffer_char('%');
-					(*fmt)++;
 					return (0);
 				}
 
-				parse_flags(fmt, &flags);
-				width = parse_width(fmt);
-				precision = parse_precision(fmt);
-				spec = **fmt;
-				if (!spec) return (1);
-				arg = (*arg_idx < arg_count) ? args[*arg_idx] : "";
-
 				if (spec == 'd' || spec == 'i') {
-					errno = 0;
-					num = strtol(arg, &endptr, 10);
-					if (*arg && (*endptr != '\0' || errno == ERANGE)) {
+					num = parse_signed_len(arg, length, &ok);
+					if (!ok) {
 						char err_msg[256];
 						snprintf(err_msg, sizeof(err_msg), "%s: printf: %s: invalid number\n", shell.name, arg);
 						write(2, err_msg, ft_strlen(err_msg));
@@ -526,32 +716,32 @@
 				}
 				else if (spec == 'u')
 				{
-					print_unsigned(strtoul(arg, NULL, 10), 10, width, precision, flags, 0);
+					print_unsigned(parse_unsigned_len(arg, length), 10, width, precision, flags, 0);
 					(*arg_idx)++;
 				}
 				else if (spec == 'o')
 				{
-					print_unsigned(strtoul(arg, NULL, 10), 8, width, precision, flags, 0);
+					print_unsigned(parse_unsigned_len(arg, length), 8, width, precision, flags, 0);
 					(*arg_idx)++;
 				}
 				else if (spec == 'x')
 				{
-					print_unsigned(strtoul(arg, NULL, 10), 16, width, precision, flags, 0);
+					print_unsigned(parse_unsigned_len(arg, length), 16, width, precision, flags, 0);
 					(*arg_idx)++;
 				}
 				else if (spec == 'X')
 				{
-					print_unsigned(strtoul(arg, NULL, 10), 16, width, precision, flags, 1);
+					print_unsigned(parse_unsigned_len(arg, length), 16, width, precision, flags, 1);
 					(*arg_idx)++;
 				}
 				else if (spec == 'f' || spec == 'F')
 				{
-					print_float(atof(arg), width, precision, flags);
+					print_float(strtod(arg, NULL), width, precision, flags);
 					(*arg_idx)++;
 				}
 				else if (spec == 'e' || spec == 'E' || spec == 'g' || spec == 'G')
 				{
-					print_float(atof(arg), width, precision, flags);
+					print_float_scientific(strtod(arg, NULL), width, precision, flags, spec);
 					(*arg_idx)++;
 				}
 				else if (spec == 's')
@@ -569,10 +759,14 @@
 				}
 				else if (spec == 'b')
 				{
-					char *processed = process_escapes(arg);
-					if (!processed) return (1);
-					print_string(processed, width, precision, flags);
-					free(processed);
+					if (has_escape(arg)) {
+						char *processed = process_escapes(arg);
+						if (!processed) return (1);
+						print_string(processed, width, precision, flags);
+						free(processed);
+					} else {
+						print_string(arg, width, precision, flags);
+					}
 					(*arg_idx)++;
 				}
 				else
@@ -581,8 +775,6 @@
 					buffer_char(spec);
 					return (1);
 				}
-				(*fmt)++;
-
 				return (0);
 			}
 
@@ -592,40 +784,48 @@
 
 	#pragma region "Main"
 
-		static int	printf_main(int argc, char **argv) {
+		static int printf_main(int argc, char **argv) {
 			char		*format;
 			char		*processed_fmt;
-			const char	*ptr;
 			int			arg_idx = 1;
 			int			ret = 0;
 			int			has_format_specs;
+			int			free_fmt;
+			t_fmt_token	*tokens;
+			size_t		count;
+			size_t		i;
 
 			g_buffer_pos = 0;
 			if (!argc) return (1);
 			format = argv[0];
 
-			processed_fmt = process_escapes(format);
-			if (!processed_fmt) return (1);
+			processed_fmt = format;
+			free_fmt = 0;
+			if (has_escape(format)) {
+				processed_fmt = process_escapes(format);
+				if (!processed_fmt) return (1);
+				free_fmt = 1;
+			}
+			if (parse_format(processed_fmt, &tokens, &count, &has_format_specs)) {
+				if (free_fmt) free(processed_fmt);
+				return (1);
+			}
 
 			do {
-				ptr = processed_fmt;
-				has_format_specs = 0;
-
-				while (*ptr) {
-					if (*ptr == '%') {
-						has_format_specs = 1;
-						if (process_specifier(&ptr, argv, &arg_idx, argc) != 0) {
+				for (i = 0; i < count; i++) {
+					if (tokens[i].is_spec) {
+						if (process_specifier_parsed(&tokens[i], argv, &arg_idx, argc) != 0) {
 							ret = 1;
 							break;
 						}
 					} else {
-						buffer_char(*ptr);
-						ptr++;
+						buffer_string(tokens[i].lit, tokens[i].lit_len);
 					}
 				}
 			} while (has_format_specs && arg_idx < argc && !ret);
 
-			free(processed_fmt);
+			free(tokens);
+			if (free_fmt) free(processed_fmt);
 			flush_buffer();
 
 			return (ret);
